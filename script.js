5497cff34db7ac9b1c14840f25e99d8e9f3040ac
Azam08 <golamazam08@gmail.com>
2024-10-09 12:09:48 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const displayMain = document.getElementById("display");
+
+function numberDisplay(input) {
+    displayMain.value +=input;
+}
+
+function reset() {
+    displayMain.value="";
+}
+
+function calculate() {
+    try {
+        displayMain.value=eval(displayMain.value);
+    }
+
+    catch(error) {
+        displayMain.value='error';
+    }
+}
+
+function clearBtn() {
+    displayMain.value = displayMain.value.toString().slice(0,-1);
+}
+
+
+const mainContainer = document.getElementById("label");
+
+mainContainer.onclick = function () {
+    document.body.classList.toggle("white--theme");
+}
+
